9225a9e47e9a4a69cd6ea11bb5ab6c30119a43d4
AlankritSrivastava <alankritsri2000@gmail.com>
2021-02-03 14:10:42 +0530

    initial commit

---
+++ ques.js
@@ -0,0 +1,117 @@
+let questions = [
+    {
+    numb: 1,
+    question: "According to the International System of Units, how many bytes are in a kilobyte of RAM?",
+    answer: "1000",
+    options: [
+        "1000",
+        "512",
+        "1024",
+        "500"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+   {
+    numb: 6,
+    question: "What amount of bits commonly equals one byte?",
+    answer: "8",
+    options: [
+       "8",
+       "1",
+       "2",
+       "64"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "How long is an IPv6 address?",
+    answer: "128 bits",
+    options: [
+        "128 bits",
+        "32 bits",
+        "64 bits",
+        "128 bytes",
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it does not get modified?",
+    answer: "Final",
+    options: [
+        "Final",
+        "Static",
+        "Private",
+        "Public"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?",
+    answer: "Apple",
+    options: [
+        "Apple",
+        "Microsoft",
+        "Atari",
+        "Commodore"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "In computing, what does LAN stand for?",
+    answer: "Local Area Network",
+    options: [
+        "Local Area Network",
+        "Long Antenna Node",
+        "Light Access Node",
+        "Land Address Navigation"
+        ]
+  },
+];
